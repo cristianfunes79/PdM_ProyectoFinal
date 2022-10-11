071979bb10a175d514d87ea69564ab635c9182fc
cristianfunes79 <cristianfunes79@gmail.com>
2022-10-11 16:30:29 -0300

    Se agrego modulo para inicializar la placa.

---
+++ Drivers/Core/Inc/board_init.h
@@ -0,0 +1,17 @@
+/*
+ * board_init.h
+ *
+ *  Created on: Oct 11, 2022
+ *      Author: cfunes
+ */
+
+#ifndef CORE_INC_BOARD_INIT_H_
+#define CORE_INC_BOARD_INIT_H_
+
+/*
+ * @brief	Inicializa el clock y la HAL de la placa
+ * @retval	None
+ */
+void board_init(void);
+
+#endif /* CORE_INC_BOARD_INIT_H_ */
